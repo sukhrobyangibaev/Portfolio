8281f2fcc502a2a5882fd9cd9f2650fc0bf23a2b
sukhrobyangibaev <sukhrobyangibaev@gmail.com>
2019-12-30 17:08:25 +0300

    add test page

---
+++ pages/test.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import Link from "next/link";
+import { NextPage } from "next";
+
+const TestPage: NextPage = () => {
+  return (
+    <div>
+      <h1>Test page</h1>
+    </div>
+  );
+};
+
+export default TestPage;
