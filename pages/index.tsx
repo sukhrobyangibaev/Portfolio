b8e6e12d09a7f6c93f02dcf20fbb5de14059fb8d
sukhrobyangibaev <sukhrobyangibaev@gmail.com>
2020-01-02 11:35:07 +0300

    add portfolio section

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,10 +11,11 @@ const IndexPage: NextPage = () => {
         <h1>Web Developer</h1>
         <div className="arrow"></div>
       </div>
+      <h2>What can i do</h2>
+
       <div className="canDo">
-        <h2>What can i do.</h2>
         <div className="row">
-          <img src="canDo/front.jpeg"></img>
+          <img className="stacks" src="canDo/front.jpeg"></img>
           <div className="info">
             Frontend
             <span>
@@ -35,10 +36,10 @@ const IndexPage: NextPage = () => {
               enough to handle large volumes of traffic.
             </span>
           </div>
-          <img src="canDo/back.png"></img>
+          <img className="stacks" src="canDo/back.png"></img>
         </div>
         <div className="row">
-          <img src="canDo/full.png"></img>
+          <img className="stacks" src="canDo/full.png"></img>
           <div className="info">
             Fullstack
             <span>
@@ -49,6 +50,62 @@ const IndexPage: NextPage = () => {
           </div>
         </div>
       </div>
+      <hr />
+      <h2>My Portfolio</h2>
+
+      <div className="container">
+        <div className="cont">
+          <a href="https://sukhrobyangibaev.github.io/Piano-Museum/">
+            <img className="portImg" src="porfolio/museum.png" />
+          </a>
+          <div className="middle">
+            <div className="text">Piano Museum</div>
+          </div>
+        </div>
+        <div className="cont">
+          <a href="https://yelp-camp-yangibaev.herokuapp.com/">
+            <img className="portImg" src="porfolio/yelp.png" />
+          </a>
+          <div className="middle">
+            <div className="text">Yelp Camp</div>
+          </div>
+        </div>
+        <div className="cont">
+          <a href="https://simple-dice.herokuapp.com/">
+            <img className="portImg" src="porfolio/dice.png" />
+          </a>
+          <div className="middle">
+            <div className="text">Simple Dice</div>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="cont">
+          <a href="https://lights-out-reac.herokuapp.com/">
+            <img className="portImg" src="porfolio/lights.png" />
+          </a>
+          <div className="middle">
+            <div className="text">Lights out</div>
+          </div>
+        </div>
+        <div className="cont">
+          <a href="https://yahtzee-next.herokuapp.com/">
+            <img className="portImg" src="porfolio/yah.png" />
+          </a>
+          <div className="middle">
+            <div className="text">Yahtzee</div>
+          </div>
+        </div>
+        <div className="cont">
+          <a href="https://classical-music-test-react.herokuapp.com/">
+            <img className="portImg" src="porfolio/cmt.png" />
+          </a>
+          <div className="middle">
+            <div className="text">Classical Music Test</div>
+          </div>
+        </div>
+      </div>
+      <hr />
       <style jsx>{`
         .middleText {
           width: 100%;
@@ -69,6 +126,11 @@ const IndexPage: NextPage = () => {
           font-size: 3em;
           letter-spacing: 10px;
           text-align: center;
+        }
+        h2 {
+          font-family: "Raleway", sans-serif;
+          text-align: center;
+          font-size: 3em;
         }
 
         .arrow:before {
@@ -111,11 +173,18 @@ const IndexPage: NextPage = () => {
             transform: translate(-10px, -10px);
           }
         }
-
+        hr {
+          margin: -5px auto 50px;
+          padding: 0;
+          height: 50px;
+          border: none;
+          border-bottom: 1px solid #1f1209;
+          box-shadow: 0 20px 20px -20px #333;
+          width: 85%;
+        }
         .canDo {
           width: 70%;
           margin: 0 auto;
-          border: 1px solid lightgrey;
           font-family: "Raleway", sans-serif;
           text-align: center;
           font-size: 2em;
@@ -136,9 +205,45 @@ const IndexPage: NextPage = () => {
           font-size: 0.8em;
           display: block;
         }
-        img {
+        .stacks {
           width: 250px;
           height: 250px;
+        }
+
+        .container {
+          width: 85%;
+          display: flex;
+          margin: 0 auto;
+          font-family: "Raleway", sans-serif;
+          text-align: center;
+          font-size: 2em;
+          font-weight: 300;
+        }
+        .portImg {
+          opacity: 1;
+          width: 100%;
+          height: 100%;
+          object-fit: cover;
+          transition: 0.5s ease;
+          backface-visibility: hidden;
+        }
+        .middle {
+          transition: 0.5s ease;
+          opacity: 0;
+          transform: translate(0%, -300%);
+        }
+
+        .cont {
+          width: 30%;
+          height: 220px;
+          margin: 1.66%;
+          cursor: pointer;
+        }
+        .cont:hover .portImg {
+          opacity: 0.3;
+        }
+        .cont:hover .middle {
+          opacity: 1;
         }
 
         @media (max-width: 1050px) {
@@ -148,7 +253,7 @@ const IndexPage: NextPage = () => {
           .info {
             font-size: 0.9em;
           }
-          img {
+          .stacks {
             width: 200px;
             height: 200px;
           }
@@ -160,7 +265,7 @@ const IndexPage: NextPage = () => {
           .info {
             font-size: 0.8em;
           }
-          img {
+          .stacks {
             width: 200px;
             height: 200px;
           }
@@ -183,7 +288,7 @@ const IndexPage: NextPage = () => {
           .info {
             padding: 10px 0;
           }
-          img {
+          .stacks {
             display: none;
           }
         }
